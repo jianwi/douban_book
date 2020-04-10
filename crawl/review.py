f7ff228c901c3e9439e69d6b14435b9d966c46f0
dujianjun <v@jianwi.cn>
2020-04-10 22:00:12 +0800

    书评详情

--- crawl/review.py
+++ crawl/review.py
@@ -1,6 +1,18 @@
 from crawl.crawl_tools import CrawlTools
 import re
 
+
 class Review():
-    def __init__(self):
-        pass
+    def __init__(self,code):
+        self.url = 'https://book.douban.com/review/' + str(code)
+        self.crawl = CrawlTools(self.url).soup()
+
+    def make(self):
+        return self.__content()
+
+    def __content(self):
+        return str(self.crawl.select('div#link-report>div.review-content')[0])
+
+
+t = Review(12425970)
+print(t.make())
